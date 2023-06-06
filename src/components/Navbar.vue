b27fecc5e34d8ddb674fd8731545c37297f51927
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-06 17:32:07 +0700

    initiate navbar with pokedex logo

---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="navbar p-2 bg-secondary">
+    <a class="navbar-brand" href="#">
+      <img src="./../assets/img/pokedexlogo.png" alt="pokedex" width="110" height="40" />
+    </a>
+  </nav>
+</template>
