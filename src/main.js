29b7d65251e79bb8682c15505758cdf1c123be9d
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-06 21:54:50 +0700

    install axios make axios global

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import 'bootstrap'
 import './assets/scss/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
+
+window.axios = axios
 
 const app = createApp(App)
 
