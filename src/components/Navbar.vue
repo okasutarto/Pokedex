6998a2d9f602a9d27b0af12bb6c07eb66f859a85
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-06 18:55:45 +0700

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,45 @@
 <template>
-  <nav class="navbar p-2 bg-secondary">
+  <!-- <nav class="navbar p-2 bg-secondary">
     <a class="navbar-brand" href="#">
       <img src="./../assets/img/pokedexlogo.png" alt="pokedex" width="110" height="40" />
     </a>
+  </nav> -->
+  <nav class="navbar navbar-expand-lg" style="background-color: #ff9524">
+    <div class="container">
+      <img
+        class="navbar-brand"
+        src="./../assets/img/pokedexlogo.png"
+        alt="pokedex"
+        width="110"
+        height="50"
+      />
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto text-white">
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">Pokémon</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white fs-5" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 </template>
