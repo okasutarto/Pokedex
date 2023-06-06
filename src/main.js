b3e36e3b25bc0b298b69a834b73217ce4653d411
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-06 18:14:35 +0700

    initiate scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
-import './assets/main.css'
+import './assets/scss/main.scss'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
