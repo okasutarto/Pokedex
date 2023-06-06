bb192ec5f6bfcec8954d3000694df6f997137595
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-06 18:56:01 +0700

    Create Homepage and add background

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>homepage</div>
+  <div class="layout layout__main--background">
+    <div class="d-flex flex-column justify-content-center text-center text-white pt-5 mt-5">
+      <div class="pt-5 mt-5">
+        <img
+          src="https://cdn.pixabay.com/photo/2019/11/27/14/06/pokemon-4657023_640.png"
+          width="150px"
+        />
+      </div>
+      <div class="container pb-5 mb-">
+        <h1 class="display-1">Welcome to the Pokédex</h1>
+        <p class="lead fs-2">Catch 'em all and become a Pokémon Master!</p>
+        <a href="#" class="btn btn-primary btn-lg shadow">Start Exploring</a>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style></style>
