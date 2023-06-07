3f665a1aeda4917f42da135109bf397d562a7fd8
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-07 20:36:14 +0700

    add fav pokemon icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- <nav class="navbar p-2 bg-secondary">
-    <a class="navbar-brand" href="#">
-      <img src="./../assets/img/pokedexlogo.png" alt="pokedex" width="110" height="40" />
-    </a>
-  </nav> -->
   <nav class="navbar navbar-expand-lg" style="background-color: #ff9524">
     <div class="container">
       <img
@@ -24,22 +19,36 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
         <ul class="navbar-nav ml-auto text-white">
           <li class="nav-item">
+            <!-- <router-link> -->
             <a class="nav-link text-white fs-5" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white fs-5" href="#">Pokémon</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white fs-5" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-white fs-5" href="#">Contact</a>
+            <!-- </router-link> -->
           </li>
         </ul>
+        <button class="button border-0 bg-transparent position-relative">
+          <i class="bi bi-box2-heart-fill text-white fs-4">
+            <span
+              class="position-absolute top-1 start-100 translate-middle badge rounded-pill bg-primary fs-6"
+            >
+              {{ pokemonStore.favPokemons }}
+            </span></i
+          >
+        </button>
       </div>
     </div>
   </nav>
 </template>
+
+<script>
+import { usePokemonStore } from '../plugins/pinia/index'
+
+export default {
+  setup() {
+    const pokemonStore = usePokemonStore()
+
+    return { pokemonStore }
+  }
+}
+</script>
