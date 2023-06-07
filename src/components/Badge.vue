484913af756d232f270a404b64c150c9b074a502
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-07 17:20:24 +0700

    create resuable badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="badge rounded-pill" :style="setBadgeColor(typeName)">
+    {{ typeName }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    typeName: String,
+    colours: Object
+  },
+  methods: {
+    setBadgeColor(typeName) {
+      const colours = Object.keys(this.colours)
+
+      for (const colour of colours) {
+        if (typeName === colour) {
+          return `background-color:${this.colours[colour]};margin:0.25rem`
+        }
+      }
+
+      return ''
+    }
+  }
+}
+</script>
