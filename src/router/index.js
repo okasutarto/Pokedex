68e1654ae389a396f48c585de0f4ea3321261568
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-07 21:07:16 +0700

    add route to fav page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import Dashboard from '../views/Dashboard.vue'
+import FavPokemon from '../views/FavPokemon.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: Dashboard
+    },
+    {
+      path: '/favourite',
+      name: 'favourite',
+      component: FavPokemon
     }
   ]
 })
