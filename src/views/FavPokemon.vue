29b40f8ae6af1b2fe4ce4cbb5b193e9813d03526
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-07 21:07:30 +0700

    success show fav Pokemon

--- src/views/FavPokemon.vue
+++ src/views/FavPokemon.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Fav Pokemons</div>
+  <div class="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-4 p-5">
+    <Card v-for="pokemons in pokemonStore.favPokemonsList" :key="pokemons" :url="pokemons" />
+  </div>
 </template>
+
+<script>
+import Card from './../components/Card.vue'
+import { usePokemonStore } from './../plugins/pinia/index'
+
+export default {
+  setup() {
+    const pokemonStore = usePokemonStore()
+    return { pokemonStore }
+  },
+  components: {
+    Card
+  }
+}
+</script>
