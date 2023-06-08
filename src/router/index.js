7156f8b878e4f12b07b62392e33a085924c8c2d7
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-08 20:16:09 +0700

    change web to hash in route

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import Dashboard from '../views/Dashboard.vue'
 import FavPokemon from '../views/FavPokemon.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
