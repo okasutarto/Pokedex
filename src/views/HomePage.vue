c94935ec62381f600cb07a612d0feffed9c6e860
Oka Sutarto Putra <okasputra@gmail.com>
2023-06-08 19:25:15 +0700

    minor fix

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="layout layout__main--background">
     <div class="d-flex flex-column justify-content-center text-center text-white pt-5 mt-5">
-      <div class="pt-5 mt-5">
-        <img
-          src="https://cdn.pixabay.com/photo/2019/11/27/14/06/pokemon-4657023_640.png"
-          width="150px"
-        />
-      </div>
-      <div class="container pb-5 mb-">
+      <div class="container pb-5">
         <h1 class="display-1">Welcome to the Pokédex</h1>
         <p class="lead fs-2">Catch 'em all and become a Pokémon Master!</p>
         <router-link to="/dashboard">
